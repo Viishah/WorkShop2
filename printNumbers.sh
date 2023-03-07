643c927c6cc7e39456a247cf246269484825a8e5
“Viishah” <vtorsalle@gmail.com>
2023-03-07 11:01:02 +0530

    Add: this is uc3 to store in directory

--- printNumbers.sh
+++ printNumbers.sh
@@ -1,8 +1,10 @@
+echo " Welcome to workshop 2"
 
-echo $((RANDOM % 10))
-echo "Welcome To Work Shop "
-for i in (1...10)
- do
-   case $i in
+declare -A selected_numbers
+for i in {1..10}; do
+  if [ $i -eq 1 ] || [ $i -eq 2 ] || [ $i -eq 4 ] || [ $i -eq 7 ] || [ $i -eq 9 ] || [ $i -eq 10 ]; then
+    selected_numbers[$i]=$i
+  fi
+done
 
- 
+echo "${selected_numbers[@]}" 
